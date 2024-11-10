1bb3557b80661b7940e3d33f092dddb645b8e0f8
Erick <Erick@THEGOAT>
2024-11-10 18:00:13 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp4/ListSystem.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class ListSys
+    {
+        /*This variable just tracks the amount of objects created subtracted by 2, then i use the index to create IDs, making it easier to delete.*/
+        static public int GlobalIndexCounter = 0;
+        //Don't ask wtf i have done, i know theres better ways to do it, but im too lazy;
+
+        public bool IsDone = false;
+        public String ObjectiveName;
+        public String ObjectiveDesc;
+        public String CreationDateAndTime;
+
+        public int index;
+
+        public ListSys(String ObjName, String ObjDesc, String CreateDateAndTime, int index)
+        {
+            ObjectiveName = ObjName;
+            ObjectiveDesc = ObjDesc;
+            CreationDateAndTime = CreateDateAndTime;
+
+            GlobalIndexCounter++;
+            this.index = index;
+
+        }
+        void done()
+        {
+            IsDone = true;
+
+        }
+
+
+
+
+    }
+    public abstract class UtilityChars 
+    {
+        public static string DoneASCII(bool done) 
+        {
+            if (done)
+            {
+                return "[X]";
+            }
+            else
+            {
+                return "[ ]";
+            }
+        }
+    }
+
+}
+
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections.Generic;
+using System;
+using ConsoleApp4;
+
+namespace code
+{
+
+    class Code
+    {
+        static void Main(String[] args)
+        {
+
+            String UserName = Start();
+            List<ListSys> list = new List<ListSys>();
+
+            Options(UserName, list);
+
+
+        }
+
+        static string Start()
+        {
+
+            String UserName = "user";
+
+
+            Console.WriteLine($"Welcome to the To-Do-List! {UserName}");
+
+            while (true)
+            {
+                if (UserName == "user")
+                {
+                    while (true)
+                    {
+                        Console.Write("What's your name? ");
+                        UserName = Console.ReadLine();
+                        Console.WriteLine($"Ok, set user as {UserName}? ");
+                        String input = Console.ReadLine().ToLower();
+
+                        if (input == "yes" || input == "y")
+                        {
+                            break;
+                        }
+                        else if (input == "no" || input == "n")
+                        {
+                            Console.WriteLine("Ok, going again! ");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Insert a valid answer! ");
+                        }
+                    }
+
+
+
+
+                }
+                return UserName;
+                Console.Clear();
+                break;
+
+
+            }
+        }
+        static void ReorganizeList(List<ListSys> List)
+        {
+            
+            for (int i = 0; i < List.Count; i++) 
+            {
+                List[i].index = i;  
+            }
+
+        }
+        static void Options(String UserName, List<ListSys> list)
+        {
+            Console.WriteLine($"Ok {UserName}, what you want to do?");
+            while (true)
+            {
+                Console.WriteLine("\tOptions: \n 1.See current To-Do's \n 2.Create a new To-Do \n 3.Delete a To-Do \n 4.Modify my To-Do's\n 5.Exit\n");
+                try
+                {
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.Clear();
+                            if (list.Any())
+                            {
+                                foreach (ListSys item in list)
+                                {
+
+                                    Console.WriteLine($"{item.ObjectiveName} \n {item.ObjectiveDesc} \n Created: {item.CreationDateAndTime} \n Is done? {UtilityChars.DoneASCII(item.IsDone)} ID: {item.index}\n");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Theres no items on the list.");
+
+                            }
+                            break;
+                        case 2:
+                            Console.Clear();
+                            Console.WriteLine("Set the name to \"/*\" cancel this operation!");
+
+                            Console.WriteLine("Set the name of your To-Do");
+                            String ObjName = Console.ReadLine();
+                            if (ObjName == "/*")
+                            {
+                                Console.Clear();
+                                break;
+                            }
+                            Console.WriteLine("Now, set the description of your To-Do");
+                            String ObjDesc = Console.ReadLine();
+
+
+                            DateTime today = DateTime.Now;
+                            ListSys Item = new ListSys(ObjName, ObjDesc, Convert.ToString(DateTime.Now), ListSys.GlobalIndexCounter);
+                            int Amount = ListSys.GlobalIndexCounter - 1;
+                            list.Add(Item);
+                            Console.Clear();
+                            Console.WriteLine($"Name: {list[Amount].ObjectiveName} \n Desc: {list[Amount].ObjectiveDesc}\n");
+                            break;
+                        case 3:
+                            Console.Clear();
+                            Console.WriteLine("Insert the ID of the Objective that you want to delete or type \"E\" to exit");
+                            string ObjChoice = Console.ReadLine();
+
+                            if (ObjChoice.ToLower() != "e")
+                            {
+
+                                list.Remove(list[Convert.ToInt32(ObjChoice)]);
+                                Console.Clear();
+                                Console.WriteLine("Item removed successfully");
+                                ListSys.GlobalIndexCounter--;
+                                ReorganizeList(list);
+                            }
+                            else
+                            {
+                                Console.Clear();
+                                break;
+                            }
+
+                            break;
+                        case 4:
+                            Console.Clear();
+                            Console.WriteLine("Type the ID of the To-Do to toggle the checkmark. Type \"E\" to cancel.");
+                            String ChoosenID = Console.ReadLine();
+                            if (ChoosenID.ToLower() != "e")
+                            {
+                                if (list[Convert.ToInt32(ChoosenID)].IsDone)
+                                {
+                                    list[Convert.ToInt32(ChoosenID)].IsDone = false;
+                                }
+                                else
+                                {
+                                    list[Convert.ToInt32(ChoosenID)].IsDone = true;
+                                }
+
+                            }
+                            else
+                            {
+                                Console.Clear();
+                                break;
+                            }
+                            break;
+                        case 5:
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("Insert valid option\n");
+                            Console.Clear();
+
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.Clear();   
+                    Console.WriteLine("Insert valid digit!");
+
+                   
+                    
+                }
+                
+            }
+
+
+        }
+    }
+}
+    
